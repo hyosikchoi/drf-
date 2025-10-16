7defb5d28074feb942ac28a10e31aa306e7ff803
paul <paul@metrex.im>
2025-10-16 16:09:05 +0900

    Mod: Custom Serializer -> Model Serializer 로 변경

--- apis/serializers/accounts_patient_serializers.py
+++ apis/serializers/accounts_patient_serializers.py
@@ -1,16 +1,18 @@
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
+from domains.accounts.models import Patient
 
-class AccountsPatientReqSerializer(serializers.Serializer):
+
+class AccountsPatientReqSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = ["description", "chart_number"]
+
     chart_number = serializers.CharField(help_text=_("차트 번호"))
-    description = serializers.CharField(help_text=_("메모"), allow_blank=True)
 
 
-class AccountsPatientResSerializer(serializers.Serializer):
-    created_at = serializers.DateTimeField(help_text=_("생성 일자"))
-    updated_at = serializers.DateTimeField(help_text=_("수정 일자"))
-    patient_uuid = serializers.UUIDField(help_text=_("환자 고유 번호"))
-    description = serializers.CharField(help_text=_("환자 설명"))
-    user_id = serializers.IntegerField(allow_null=True, help_text=_("의사 아이디"))
-    is_deleted = serializers.BooleanField(default=False, help_text=_("삭제 여부"))
+class AccountsPatientResSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        exclude = ["patient_uuid"]
